497d55670648875d3739b5a76b8368aa514214b1
MRamadhan000 <mcahya830@gmail.com>
2026-09-16 22:03:44 +0700

    feat : update base urll sttas

--- services/stats.service.ts
+++ services/stats.service.ts
@@ -1,9 +1,12 @@
 import { StatsResponse } from "../types/stats.type";
 
 export const fetchDashboardStats = async (): Promise<StatsResponse> => {
-  const response = await fetch("http://localhost:3000/stats", {
-    cache: "no-store", // Memastikan data selalu fresh saat dipanggil
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/stats`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Gagal mengambil data statistik");
